1ef20b5eac8acb4abc276d293f869950184b0a31
Adam Heins <mail@adamheins.com>
2016-06-26 03:23:14 -0700

    Maintain constant color contrast.

--- mandelbrot/mandelbrot.js
+++ mandelbrot/mandelbrot.js
@@ -31,7 +31,7 @@ function Mandelbrot(x, y, w, h) {
     this.magnification = 1;
 }
 
-Mandelbrot.prototype.plot = function(xP, yP, ctx) {
+Mandelbrot.prototype.plot = function(xP, yP, ctx, minIter, maxIter) {
     // Scale coordinates to lie within the Mandelbrot scale.
     var x0 = (xP) * 3.5 / this.magnification / this.w + this.offsetX;
     var y0 = (yP) * 2.0 / this.magnification / this.h + this.offsetY;
@@ -54,8 +54,8 @@ Mandelbrot.prototype.plot = function(xP, yP, ctx) {
       iter = iter + 1 - nu;
     }
 
-    var a = Math.floor(Math.cbrt(Math.floor(iter)) * 255 / Math.cbrt(MAX_ITERATIONS));
-    var b = Math.floor(Math.cbrt(Math.floor(iter) + 1) * 255 / Math.cbrt(MAX_ITERATIONS));
+    var a = Math.floor(Math.sqrt(iter - minIter) * 255 / Math.sqrt(maxIter - minIter));
+    var b = Math.floor(Math.sqrt(iter - minIter) * 255 / Math.sqrt(maxIter - minIter));
     var c = Math.floor(interpolate(a, b, iter % 1));
     var color = 'rgb(' + c + ',' + c + ',' + c + ')';
 
@@ -64,10 +64,52 @@ Mandelbrot.prototype.plot = function(xP, yP, ctx) {
     ctx.fillRect(xP, yP, 1, 1);
 }
 
+Mandelbrot.prototype.findIter = function(xP, yP, ctx) {
+    // Scale coordinates to lie within the Mandelbrot scale.
+    var x0 = (xP) * 3.5 / this.magnification / this.w + this.offsetX;
+    var y0 = (yP) * 2.0 / this.magnification / this.h + this.offsetY;
+
+    var x = 0;
+    var y = 0;
+
+    // The number of iterations we reach determines the color of the pixel.
+    var iter = 0;
+    while (x * x + y * y < BAILOUT_RADIUS && iter < MAX_ITERATIONS) {
+        var xTemp = x * x - y * y + x0;
+        y = 2 * x * y + y0;
+        x = xTemp;
+        ++iter;
+    }
+
+    if (iter < MAX_ITERATIONS) {
+      var log_zn = Math.log10(x * x + y * y) / 2;
+      var nu = Math.log10(log_zn / Math.log10(2)) / Math.log10(2);
+      iter = iter + 1 - nu;
+    }
+
+    return iter;
+}
+
+
 Mandelbrot.prototype.draw = function(ctx) {
+    var minIter = MAX_ITERATIONS;
+    var maxIter = 0;
     for (var i = this.x; i < this.w + this.x; ++i) {
         for (var j = this.y; j < this.h + this.y; ++j) {
-            this.plot(i, j, ctx);
+            let iter = this.findIter(i, j, ctx);
+            if (iter < minIter) {
+                minIter = iter;
+            } else if (iter > maxIter) {
+                maxIter = iter;
+            }
+        }
+    }
+
+    console.log(minIter + ' ' + maxIter);
+
+    for (var i = this.x; i < this.w + this.x; ++i) {
+        for (var j = this.y; j < this.h + this.y; ++j) {
+            this.plot(i, j, ctx, minIter, maxIter);
         }
     }
 }
