8ad9859aa1272007afb0bdabb8c7c170cc7a9e8f
Adam Heins <mail@adamheins.com>
2016-09-05 16:50:24 -0400

    Correct the Koch implementation.
    The previous Koch implementation was not in fact correct Koch snowflake.
    This commit corrects the implementation.

--- koch/koch.js
+++ koch/koch.js
@@ -1,95 +1,101 @@
 'use strict';
 
 let MAX_DEPTH = 5;
-let INITIAL_RADIUS = 200;
+
+let INITIAL_LENGTH = 400;
+let INITIAL_HEIGHT = Math.sqrt(3) / 2 * INITIAL_LENGTH;
 
 let DEG30 = Math.PI / 6;
 let DEG60 = Math.PI / 3;
-let DEG90 = Math.PI / 2;
-let DEG120 = Math.PI * 2 / 3;
 let DEG150 = Math.PI * 5 / 6;
 let DEG270 = Math.PI * 1.5;
 
-// Represents a triangle in the snowflake. The `orientation` parameter is the
-// angle of any of the triangle's three points from center.
-function Triangle(x, y, r, orientation, childOrientations) {
-    this.x = x; // x-coordinate of center point.
-    this.y = y; // y-coordinate of center point.
-    this.r = r; // Radius (center point to tip).
-
-    this.orientations = [orientation, orientation + DEG120,
-                         orientation + 2 * DEG120];
-
-    // `childOrientations` may be passed in, otherwise they will be calculated
-    // here. This allows for the root triangle to be constructed with
-    // manually-specified child orientations.
-    if (childOrientations === undefined) {
-        this.childOrientations = [orientation + DEG60, orientation - DEG60];
-    } else {
-        this.childOrientations = childOrientations;
-    }
+function Point(x, y) {
+    this.x = x;
+    this.y = y;
 }
 
-// Generate the triangle's child triangles.
-Triangle.prototype.spawn = function() {
-    let orientations = this.orientations.concat(this.childOrientations);
-
-    let children = orientations.map(orientation => {
-        let childX = this.x + this.r * 2 / 3 * Math.cos(orientation);
-        let childY = this.y + this.r * 2 / 3 * Math.sin(orientation);
-        let childR = this.r / 3;
-
-        return new Triangle(childX, childY, childR, orientation);
-    });
-
-    return children;
-}
-
-// Draw the triangle.
-Triangle.prototype.draw = function(ctx, color) {
-    ctx.fillStyle = color;
+// Render the fractal.
+function render(ctx, points, color) {
+    ctx.strokeStyle = color;
     ctx.beginPath();
-    ctx.moveTo(this.x + this.r * Math.cos(this.orientations[0]),
-               this.y + this.r * Math.sin(this.orientations[0]));
-    ctx.lineTo(this.x + this.r * Math.cos(this.orientations[1]),
-               this.y + this.r * Math.sin(this.orientations[1]));
-    ctx.lineTo(this.x + this.r * Math.cos(this.orientations[2]),
-               this.y + this.r * Math.sin(this.orientations[2]));
-    ctx.fill();
+    ctx.moveTo(points[points.length - 1].x, points[points.length - 1].y);
+    points.forEach(point => {
+        ctx.lineTo(point.x, point.y);
+    });
+    ctx.stroke();
 }
 
-// Render the triangles.
-function render(triangles) {
-    triangles.forEach(triangle => {
-        triangle.draw(ctx, 'black');
-    });
+// Create the new generation of points and angles between one pair of points.
+function stepOne(pointA, pointB, angle) {
+    let dx = pointB.x - pointA.x;
+    let dy = pointB.y - pointA.y;
+    let length = Math.sqrt(dx * dx + dy * dy);
+
+    // Points various fractions of the way along the line between `pointA` and
+    // `pointB`.
+    let point13 = new Point(pointA.x + dx / 3,  pointA.y + dy / 3);
+    let point12 = new Point(pointA.x + dx / 2, pointA.y + dy / 2);
+    let point23 = new Point(pointA.x + dx * 2 / 3, pointA.y + dy * 2 / 3);
+
+    // The point at the tip of the "spike".
+    let pointSpike = new Point(point12.x + length * Math.cos(angle) / 3,
+                               point12.y - length * Math.sin(angle) / 3);
+
+    // We've chosen to include `pointA` in the returned array of new points,
+    // rather than `pointB`. It must be one or the other, not both, to avoid
+    // duplication of points.
+    return {
+        'points': [pointA, point13, pointSpike, point23],
+        'angles': [angle, angle + DEG60, angle - DEG60, angle]
+    };
 }
 
-// Generate the new generation of triangles.
-function more(triangles) {
-    let children = [];
-    triangles.forEach(triangle => {
-        let spawn = triangle.spawn();
-        children = children.concat(spawn);
-    });
-    return children;
+// Create the new generation of points and angles.
+function step(points, angles) {
+    let childPoints = [];
+    let childAngles = [];
+
+    for (let i = 1; i < points.length; ++i) {
+        let values = stepOne(points[i - 1], points[i], angles[i - 1]);
+        childPoints = childPoints.concat(values['points']);
+        childAngles = childAngles.concat(values['angles']);
+    }
+    let values = stepOne(points[points.length - 1], points[0],
+                         angles[angles.length - 1]);
+    childPoints = childPoints.concat(values['points']);
+    childAngles = childAngles.concat(values['angles']);
+
+    return {
+        'points': childPoints,
+        'angles': childAngles
+    };
 }
 
 let canvas = document.getElementById('kochSnowflake');
 let ctx = canvas.getContext('2d');
 
-// The starting triangle.
-let root = new Triangle(canvas.width / 2, canvas.height / 2, INITIAL_RADIUS,
-                        DEG90, [DEG30, DEG150, DEG270]);
-let triangles = [root];
+let cX = canvas.width / 2;
+let cY = canvas.height / 2;
+
+// The initial set of points defines a simple equilateral triangle.
+let points = [new Point(cX, cY - INITIAL_HEIGHT * 2 / 3),
+              new Point(cX + INITIAL_LENGTH / 2, cY + INITIAL_HEIGHT / 3),
+              new Point(cX - INITIAL_LENGTH / 2, cY + INITIAL_HEIGHT / 3)];
+let angles = [DEG30, DEG270, DEG150];
 let depth = 0;
 
-render(triangles);
+render(ctx, points, 'black');
 
 canvas.addEventListener('click', () => {
     if (depth < MAX_DEPTH) {
-        triangles = more(triangles);
-        render(triangles);
+        let values = step(points, angles);
+        points = values['points'];
+        angles = values['angles'];
+
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        render(ctx, points, 'black');
+
         ++depth;
     }
 });
